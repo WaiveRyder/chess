a0a36b578e42b370915c008bc94d35ef04d7c71c
WaiveRyder <nzh.wolf@gmail.com>
2026-01-16 11:54:52 -0700

    Added pieceColor and type to ChessPiece and added to initializer

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,7 +10,13 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
+
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
