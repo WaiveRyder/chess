1919d1ed8924b0a16712a768ab5e0ff98ffca4d4
WaiveRyder <nzh.wolf@gmail.com>
2026-01-16 22:34:23 -0700

    Implemented King potential movement

---
+++ shared/src/main/java/chess/KingMove.java
@@ -0,0 +1,57 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class KingMove {
+
+    private ChessBoard board;
+    private ChessPosition myPosition;
+    private ChessGame.TeamColor color;
+    private ArrayList<ChessMove> kingMoves;
+
+    public KingMove(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
+        this.board = board;
+        this.myPosition = myPosition;
+        this.color = color;
+    }
+
+    public Collection<ChessMove> getKingMoves(){
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+        int[] grid = {-1, 0, 1};
+
+        kingMoves = new ArrayList<ChessMove>();
+
+        for(int row : grid){
+            for(int col : grid){
+                if(row == 0 && col == 0){
+                    continue;
+                }
+                if((myRow+row > 0 && myRow+row < 9) && (myCol+col > 0 && myCol+col < 9)){
+                    ChessPosition newPosition = new ChessPosition(myRow+row, myCol+col);
+                    if(board.getPiece(newPosition) == null || board.getPiece(newPosition).getTeamColor() != color){
+                        kingMoves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+
+                }
+            }
+        }
+        return kingMoves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        KingMove kingMove = (KingMove) o;
+        return Objects.equals(board, kingMove.board) && Objects.equals(myPosition, kingMove.myPosition) && color == kingMove.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, myPosition, color);
+    }
+}
