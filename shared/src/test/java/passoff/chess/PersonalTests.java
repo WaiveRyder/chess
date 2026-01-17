66f6b077fcb2263eb38b515d959dea9280f4a5d6
WaiveRyder <nzh.wolf@gmail.com>
2026-01-16 19:16:35 -0700

    Added test for the potential pawn moves

--- shared/src/test/java/passoff/chess/PersonalTests.java
+++ shared/src/test/java/passoff/chess/PersonalTests.java
@@ -1,9 +1,6 @@
 package passoff.chess;
 
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessPiece;
-import chess.ChessPosition;
+import chess.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,5 +43,21 @@ public class PersonalTests {
                 |P|P|P|P|P|P|P|P|
                 |R|N|B|Q|K|B|N|R|""";
         Assertions.assertEquals(expectedBoard, actualBoard);
+    }
+
+    @Test
+    @DisplayName("PawnMove One Space")
+    public void pawnMoveOneSpace(){
+        ChessBoard board = new ChessBoard();
+        board.resetBoard();
+
+        ChessPosition position = new ChessPosition(2, 2);
+        Collection<ChessMove> actualMoves = board.getPiece(position).pieceMoves(board, position);
+
+        Collection<ChessMove> expectedMoves = new ArrayList<ChessMove>();
+        expectedMoves.add(new ChessMove(new ChessPosition(2, 2), new ChessPosition(3, 2), null));
+        expectedMoves.add(new ChessMove(new ChessPosition(2, 2), new ChessPosition(4, 2), null));
+
+        Assertions.assertEquals(expectedMoves, actualMoves);
     }
 }
