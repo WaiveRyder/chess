4dec5c74114cb60b520e513943392c1b3a6f365b
WaiveRyder <nzh.wolf@gmail.com>
2026-02-02 09:51:11 -0700

    Implemented simple validMoves function, returns all moves (ignores checks)

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -51,7 +51,12 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece piece = board.getPiece(startPosition);
+        if(piece != null) {
+            return piece.pieceMoves(board, startPosition);
+        } else {
+            return null;
+        }
     }
 
     /**
