845b27c05ed040d78af99bd81e0b3f2bbcdc914a
WaiveRyder <nzh.wolf@gmail.com>
2026-02-02 12:18:22 -0700

    Implemented isInCheck function

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Vector;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -13,10 +14,23 @@ public class ChessGame {
     ChessBoard board;
     TeamColor teamTurn;
 
+    Vector<ChessPiece.PieceType> QRList;
+    Vector<ChessPiece.PieceType> QBPList;
+    Vector<ChessPiece.PieceType> NList;
+
+    int[] gridRowQR;
+    int[] gridColQR;
+    int[] gridRowQBP;
+    int[] gridColQBP;
+    int[] gridRowN;
+    int[] gridColN;
+
     public ChessGame() {
         board = new ChessBoard();
         setBoard(board);
         teamTurn = TeamColor.WHITE;
+        initCoords();
+        initLists();
     }
 
     /**
@@ -76,6 +90,9 @@ public class ChessGame {
                     " to " + endPosition;
             throw new InvalidMoveException(builder);
         } else {
+            if(move.promotionPiece != null){
+                piece = new ChessPiece(piece.getTeamColor(), move.promotionPiece);
+            }
             board.addPiece(endPosition, piece);
             board.addPiece(startPosition, null);
         }
@@ -88,7 +105,63 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        ChessPosition kingPos = findKingPos(teamColor);
+
+        if(kingPos == null) {return false;}
+
+        return kingVectors(kingPos, teamColor, QRList, gridRowQR, gridColQR)
+                || kingVectors(kingPos, teamColor, NList, gridRowN, gridColN)
+                || kingVectors(kingPos, teamColor, QBPList, gridRowQBP, gridColQBP);
+    }
+
+    private boolean kingVectors(ChessPosition pos, TeamColor color, Vector<ChessPiece.PieceType> pieceList, int[] row, int[] col) {
+        int newRow;
+        int newCol;
+        int myRow = pos.getRow();
+        int myCol = pos.getColumn();
+        for(int i = 0; i < row.length; i++) {
+            newRow = myRow + row[i];
+            newCol = myCol + col[i];
+            while(newRow < 9 && newRow > 0 && newCol < 9 && newCol > 0){
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece newPiece = board.getPiece(newPos);
+
+                if(newPiece != null
+                        && newPiece.getTeamColor() != color
+                        && pieceList.contains(newPiece.getPieceType())) {
+                    if (newPiece.getPieceType() == ChessPiece.PieceType.PAWN) {
+                        if (color == TeamColor.WHITE && newRow - myRow == 1 && col[i] != 0) {
+                            return true;
+                        } else if(color == TeamColor.BLACK && myRow-newRow == 1 && col[i] != 0) {
+                            return true;
+                        }
+                    } else {
+                        return true;
+                    }
+                } else if(newPiece != null) {
+                    break;
+                }
+
+                newRow += row[i];
+                newCol += col[i];
+            }
+        }
+
+        return false;
+    }
+
+    private ChessPosition findKingPos(TeamColor color) {
+        for(int row = 1; row < 9; row++) {
+            for(int col = 1; col < 9; col++) {
+                ChessPosition newPos = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(newPos);
+                if(piece != null && piece.getPieceType() == ChessPiece.PieceType.KING && piece.getTeamColor() == color) {
+                    return newPos;
+                }
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -118,7 +191,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        board.resetBoard();
+        this.board = board;
     }
 
     /**
@@ -128,5 +201,30 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return board;
+    }
+
+    private void initCoords() {
+        gridRowQR = new int[]{0, 1, 0, -1};
+        gridColQR = new int[]{-1, 0, 1, 0};
+
+        gridRowQBP = new int[]{1, 1, -1, -1};
+        gridColQBP = new int[]{-1, 1, 1, -1};
+
+        gridRowN = new int[]{1, 2, 2, 1, -1, -2, -2, -1};
+        gridColN = new int[]{-2, -1, 1, 2, 2, 1, -1, -2};
+    }
+
+    private void initLists() {
+        QRList = new Vector<>();
+        QRList.add(ChessPiece.PieceType.QUEEN);
+        QRList.add(ChessPiece.PieceType.ROOK);
+
+        QBPList = new Vector<>();
+        QBPList.add(ChessPiece.PieceType.QUEEN);
+        QBPList.add(ChessPiece.PieceType.BISHOP);
+        QBPList.add(ChessPiece.PieceType.PAWN);
+
+        NList = new Vector<>();
+        NList.add(ChessPiece.PieceType.KNIGHT);
     }
 }
