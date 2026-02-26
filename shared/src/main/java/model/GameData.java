d54761be18e71057c107aa6c1d4d6075cc6a08cb
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 16:36:35 -0700

    Removed the gameID counter and moved it to the GameDAO

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,17 +4,6 @@ import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
-    private static int gameIDCounter = 0;
-
-    public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-        this(gameIDCounter, whiteUsername, blackUsername, gameName, game);
-        incrementGameID();
-    }
-
-    private void incrementGameID() {
-        gameIDCounter++;
-    }
-
     GameData addWhitePlayer(String newWhitePlayer) {
         return new GameData((gameID), newWhitePlayer, (blackUsername), (gameName), (game));
     }
