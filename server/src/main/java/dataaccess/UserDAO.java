af0b9cb8023b20fb4892586ff5979898b016ed03
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 16:31:49 -0700

    Changed naming of variables in UserDao

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -6,22 +6,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserDAO {
-    private Map<String, UserData> usersMap;
+    private Map<String, UserData> userMap;
 
     public UserDAO() {
-        usersMap = new HashMap<String, UserData>();
+        userMap = new HashMap<String, UserData>();
     }
 
-    public void createUser(String username, String password, String email) throws DataAccessException {
-        if (usersMap.containsKey(username)) {
+    public UserData createUser(String username, String password, String email) throws DataAccessException {
+        if (userMap.containsKey(username)) {
             throw new DataAccessException("Database already contains username: " + username);
         } else {
-            usersMap.put(username, new UserData(username, password, email));
+            UserData newUser = new UserData(username, password, email);
+            userMap.put(username, newUser);
+            return newUser;
         }
     }
 
     public UserData getUser(String username) throws DataAccessException {
-        UserData user = usersMap.get(username);
+        UserData user = userMap.get(username);
         if (user == null) {
             throw new DataAccessException("Database does not contain a user called: " + username);
         } else {
@@ -30,6 +32,6 @@ public class UserDAO {
     }
 
     public void clear() {
-        usersMap = new HashMap<String, UserData>();
+        userMap = new HashMap<String, UserData>();
     }
 }
