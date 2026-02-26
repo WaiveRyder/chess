962f5d9e6e3481dc1f20a8ec209df6615d5c936c
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 16:31:37 -0700

    Implemented AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,42 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class AuthDAO {
+    private Map<String, AuthData> authMap;
+
+    public AuthDAO() {
+        authMap = new HashMap<>();
+    }
+
+    public AuthData createAuth(UserData user) {
+        return new AuthData(UUID.randomUUID().toString(), user.username());
+    }
+
+    public AuthData getAuthData(String token) throws DataAccessException {
+        AuthData auth = authMap.get(token);
+        if(auth == null) {
+            throw new DataAccessException("Given token is not valid");
+        } else {
+            return auth;
+        }
+    }
+
+    public void deleteAuthData(String token) throws DataAccessException {
+        AuthData auth = authMap.get(token);
+        if(auth == null) {
+            throw new DataAccessException("Given token is not valid");
+        } else {
+            authMap.remove(token);
+        }
+    }
+
+    public void clear() {
+        authMap = new HashMap<>();
+    }
 }
