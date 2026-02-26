8135d9df961ebf7e1ba5a4f7fd01a8bd618621e8
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 08:34:58 -0700

    Implemented GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,25 @@
 package model;
 
-public class GameData {
+import chess.ChessGame;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+
+    private static int gameIDCounter = 0;
+
+    public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this(gameIDCounter, whiteUsername, blackUsername, gameName, game);
+        incrementGameID();
+    }
+
+    private void incrementGameID() {
+        gameIDCounter++;
+    }
+
+    GameData addWhitePlayer(String newWhitePlayer) {
+        return new GameData((gameID), newWhitePlayer, (blackUsername), (gameName), (game));
+    }
+
+    GameData addBlackPlayer(String newBlackPlayer) {
+        return new GameData((gameID), (whiteUsername), newBlackPlayer, (gameName), (game));
+    }
 }
