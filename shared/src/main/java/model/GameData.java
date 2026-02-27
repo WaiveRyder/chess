c08086c46888d48212c043c508d86571329269a2
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 17:37:05 -0700

    Changed method names

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,11 +4,11 @@ import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
-    GameData addWhitePlayer(String newWhitePlayer) {
+    GameData setWhitePlayer(String newWhitePlayer) {
         return new GameData((gameID), newWhitePlayer, (blackUsername), (gameName), (game));
     }
 
-    GameData addBlackPlayer(String newBlackPlayer) {
+    GameData setBlackPlayer(String newBlackPlayer) {
         return new GameData((gameID), (whiteUsername), newBlackPlayer, (gameName), (game));
     }
 }
