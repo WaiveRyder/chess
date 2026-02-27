edc70e9578a18868ff60fc4d656cf6cae3018b44
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 17:38:00 -0700

    Made methods public in GameData

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,11 +4,11 @@ import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
-    GameData setWhitePlayer(String newWhitePlayer) {
+    public GameData setWhitePlayer(String newWhitePlayer) {
         return new GameData((gameID), newWhitePlayer, (blackUsername), (gameName), (game));
     }
 
-    GameData setBlackPlayer(String newBlackPlayer) {
+    public GameData setBlackPlayer(String newBlackPlayer) {
         return new GameData((gameID), (whiteUsername), newBlackPlayer, (gameName), (game));
     }
 }
