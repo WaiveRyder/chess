87ba9196db7d29c1788529df73e0fb63374048fa
WaiveRyder <nzh.wolf@gmail.com>
2026-02-26 17:49:08 -0700

    Implemented GameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,57 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
 public class GameDAO {
+    private Map<Integer, GameData> gameMap;
+    private static int gameID;
+
+    public GameDAO() {
+        gameMap = new HashMap<>();
+        gameID = 0;
+    }
+
+    private void increaseGameID() {
+        gameID++;
+    }
+
+    public GameData createGame(String gameName) {
+        ChessGame chessGame = new ChessGame();
+        GameData game = new GameData(gameID, null, null, gameName, chessGame);
+        gameMap.put(gameID, game);
+        increaseGameID();
+        return game;
+    }
+
+    public GameData getGame(int id) throws DataAccessException {
+        GameData game = gameMap.get(id);
+        if (game == null) {
+            throw new DataAccessException("Cannot get game because game ID does not map to a game: " + id);
+        } else {
+            return game;
+        }
+    }
+
+    public Collection<GameData> listGames() {
+        return gameMap.values();
+    }
+
+    public GameData joinGame(int id, String username) throws DataAccessException {
+        GameData game = gameMap.get(id);
+        if (game == null) {
+            throw new DataAccessException("Cannot join game because game ID does not map to a game: " + id);
+        } else if (game.whiteUsername() != null) {
+            return game.setWhitePlayer(username);
+        } else if (game.blackUsername() != null) {
+            return game.setBlackPlayer(username);
+        } else {
+            throw new DataAccessException("Cannot join game because game is full: " + id);
+        }
+    }
 }
