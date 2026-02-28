933f1e77b16329c4f6655da98c356ddf73813087
WaiveRyder <nzh.wolf@gmail.com>
2026-02-27 17:07:40 -0700

    Changed gameID to an int, updated Game Service to use Requests Records

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -5,6 +5,9 @@ import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import model.AuthData;
 import model.GameData;
+import service.requests.AuthRequest;
+import service.requests.CreateGameRequest;
+import service.requests.JoinGameRequest;
 
 public class GameService {
     AuthDAO authDAO;
@@ -23,29 +26,29 @@ public class GameService {
         return authDAO.getAuthData(token);
     }
 
-    public RequestAndResponse createGame(RequestAndResponse request) {
+    public RequestAndResponse createGame(CreateGameRequest request) {
         try {
-            AuthData auth = authenticate(request.getAuthToken());
-            GameData newGame = gameDAO.createGame(request.getGameName());
+            AuthData auth = authenticate(request.authToken());
+            GameData newGame = gameDAO.createGame(request.gameName());
             return new RequestAndResponse().setGameID(newGame.gameID());
         } catch (DataAccessException e) {
             return new RequestAndResponse().setErrorMessage(e.getMessage());
         }
     }
 
-    public RequestAndResponse joinGame(RequestAndResponse request) {
+    public RequestAndResponse joinGame(JoinGameRequest request) {
         try {
-            AuthData auth = authenticate(request.getAuthToken());
-            GameData game = gameDAO.joinGame(request.getGameID(), auth.username(), request.getPlayerColor());
+            AuthData auth = authenticate(request.authToken());
+            GameData game = gameDAO.joinGame(request.gameID(), auth.username(), request.playerColor());
             return new RequestAndResponse();
         } catch (DataAccessException e) {
             return new RequestAndResponse().setErrorMessage(e.getMessage());
         }
     }
 
-    public RequestAndResponse listGames(RequestAndResponse request) {
+    public RequestAndResponse listGames(AuthRequest request) {
         try {
-            AuthData auth = authenticate(request.getAuthToken());
+            AuthData auth = authenticate(request.authToken());
             return new RequestAndResponse().setGamesList(gameDAO.listGames());
         } catch (DataAccessException e) {
             return new RequestAndResponse().setErrorMessage(e.getMessage());
--- server/src/main/java/service/requests/JoinGameRequest.java
+++ server/src/main/java/service/requests/JoinGameRequest.java
@@ -1,3 +1,3 @@
 package service.requests;
 
-public record JoinGameRequest(String authToken, String playerColor, String gameID) {}
+public record JoinGameRequest(String authToken, String playerColor, int gameID) {}
