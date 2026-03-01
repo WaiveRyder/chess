38ba83db5d1b5d80805e8d8707e94451f96ce104
WaiveRyder <nzh.wolf@gmail.com>
2026-03-01 13:19:13 -0700

    Implemented /user path

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,18 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
 import io.javalin.*;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import service.GameService;
+import service.UserService;
+import service.requests.RegisterRequest;
+import service.responses.AuthResponse;
+
+import java.util.Objects;
 
 public class Server {
 
@@ -10,6 +22,29 @@ public class Server {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        UserDAO userDAO = new UserDAO();
+        AuthDAO authDAO = new AuthDAO();
+        GameDAO gameDAO = new GameDAO();
+
+        GameService gameService = new GameService(authDAO, gameDAO);
+        UserService userService = new UserService(userDAO, authDAO);
+
+        var serializer = new Gson();
+
+        javalin.post("/user", new Handler() {
+            public void handle(Context context) {
+                RegisterRequest request = serializer.fromJson(context.body(), RegisterRequest.class);
+                AuthResponse response = userService.registerUser(request);
+
+                context.contentType("application/json");
+                context.result(serializer.toJson(response));
+                if(Objects.equals(response.message(), "")) {
+                    context.status(200);
+                } else {
+                    context.status(400);
+                }
+            }
+        });
 
     }
 
