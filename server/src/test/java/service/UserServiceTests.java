7adee241bd0e0d93db1e20ae769574c696560939
WaiveRyder <nzh.wolf@gmail.com>
2026-03-01 18:32:36 -0700

    Updated UserServiceTests to be more robust

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -2,6 +2,8 @@ package service;
 
 import dataaccess.AuthDAO;
 import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,12 @@ import service.requests.RegisterRequest;
 import service.responses.AuthResponse;
 import service.responses.GenericResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserServiceTests {
-    private UserDAO userDao;
-    private AuthDAO authDAO;
+    private Map<String, AuthData> authMap;
+    private Map<String, UserData> userMap;
 
     private UserService userService;
 
@@ -21,8 +26,11 @@ public class UserServiceTests {
 
     @BeforeEach
     public void setup() {
-        userDao = new UserDAO();
-        authDAO = new AuthDAO();
+        authMap = new HashMap<>();
+        userMap = new HashMap<>();
+
+        UserDAO userDao = new UserDAO(userMap);
+        AuthDAO authDAO = new AuthDAO(authMap);
 
         userService = new UserService(userDao, authDAO);
 
@@ -37,19 +45,36 @@ public class UserServiceTests {
 
         AuthResponse expected = new AuthResponse("John", actual.authToken(), "");
 
+        UserData expectedData = new UserData("John", "password", "john@email.com");
+        UserData actualData = userMap.get("John");
+
         Assertions.assertEquals(expected, actual);
         Assertions.assertNotEquals("", actual.authToken());
+        Assertions.assertNotNull(authMap.get(actual.authToken()));
+        Assertions.assertEquals(expectedData, actualData);
     }
 
     @Test
     public void registerNewUserOldInfo() {
-        RegisterRequest request = new RegisterRequest("Wally", "passwords", "wally@gmail.com");
+        RegisterRequest request = new RegisterRequest("Wally", "wowza", "wally@hotmail.com");
 
         AuthResponse actual = userService.registerUser(request);
 
-        AuthResponse expected = new AuthResponse("", "", "Database already contains username: Wally");
+        AuthResponse expected = new AuthResponse(null, null, "Error: Database already contains username: Wally");
+
+        int expectedLength = 1;
+        int actualLength = userMap.size();
+
+        UserData expectedData = new UserData("Wally", "passwords", "wally@gmail.com");
+        UserData acutalData = userMap.get("Wally");
+
+        int expectedAuthLength = 1;
+        int actualAuthLength = authMap.size();
 
         Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expectedLength, actualLength);
+        Assertions.assertEquals(expectedData, acutalData);
+        Assertions.assertEquals(expectedAuthLength, actualAuthLength);
     }
 
     @Test
@@ -58,33 +83,42 @@ public class UserServiceTests {
 
         AuthResponse expected = new AuthResponse("Wally", actual.authToken(), "");
 
+        AuthData expectedData = new AuthData(actual.authToken(), "Wally");
+        AuthData actualData = authMap.get(expected.authToken());
+
         Assertions.assertEquals(expected, actual);
         Assertions.assertNotEquals("", actual.authToken());
+        Assertions.assertEquals(expectedData, actualData);
     }
 
     @Test
     public void loginWithBadInfo() {
         AuthResponse actual = userService.loginUser(new LoginRequest("Wall", "passwords"));
 
-        AuthResponse expected = new AuthResponse("", "", "Database does not contain a user called: Wall");
+        AuthResponse expected = new AuthResponse(null, null, "Error: Database does not contain a user called: Wall");
+
+        int expectedLength = 1;
+        int actualLength = authMap.size();
 
         Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expectedLength, actualLength);
     }
 
     @Test
     public void logoutUserWithValidToken() {
         GenericResponse actual = userService.logoutUser(new AuthRequest(mockUserAuthData.authToken()));
 
-        GenericResponse expected = new GenericResponse("");
+        GenericResponse expected = new GenericResponse(null);
 
         Assertions.assertEquals(expected, actual);
+        Assertions.assertNull(authMap.get(mockUserAuthData.authToken()));
     }
 
     @Test
     public void logoutUserWithInvalidToken() {
         GenericResponse actual = userService.logoutUser(new AuthRequest("ThisIsAnAuthTokenTrustMe"));
 
-        GenericResponse expected = new GenericResponse("Given token is not valid");
+        GenericResponse expected = new GenericResponse("Error: Given token is not valid");
 
         Assertions.assertEquals(expected, actual);
     }
@@ -93,6 +127,7 @@ public class UserServiceTests {
     public void clearDatabase() {
         userService.clear();
 
-
+        Assertions.assertTrue(authMap.isEmpty());
+        Assertions.assertTrue(userMap.isEmpty());
     }
 }
