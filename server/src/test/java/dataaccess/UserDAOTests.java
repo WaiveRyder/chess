081c02be00a3d31e4f527fe94b106c4d7cf4d598
WaiveRyder <nzh.wolf@gmail.com>
2026-03-07 14:45:25 -0700

    Implemented negative test case for create user

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAOTests {
@@ -42,12 +44,40 @@ public class UserDAOTests {
             try (var conn = DatabaseManager.getConnection();
                  var pstmt = conn.prepareStatement(statement)) {
                 pstmt.setString(1, "John");
-                try (var rs = pstmt.executeQuery()) {
+                try (ResultSet rs = pstmt.executeQuery()) {
                     Assertions.assertTrue(rs.next());
                     Assertions.assertEquals("John", rs.getString("username"));
                     Assertions.assertEquals(actual.password(), rs.getString("password"));
                     Assertions.assertEquals("email", rs.getString("email"));
                 }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void createUserInvalidInfo() {
+        try {
+            userDAO.createUser("John", "password", "email");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            userDAO.createUser("John", "password", "different");
+            Assertions.fail("Expected an error to be thrown here, user already exists");
+        } catch (DataAccessException e) {
+            Assertions.assertEquals("Error: Database already contains username: John", e.getMessage());
+        }
+
+        var statement = "SELECT * FROM users WHERE username = ?";
+        try (var conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(statement)) {
+            pstmt.setString(1, "John");
+            try (ResultSet rs = pstmt.executeQuery()) {
+                Assertions.assertTrue(rs.next());
+                Assertions.assertFalse(rs.next());
             }
         } catch (SQLException | DataAccessException e) {
             throw new RuntimeException(e);
