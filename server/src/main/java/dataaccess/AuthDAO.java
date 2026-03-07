7ca2f894f12fab588db7afbe6838943c77bf6684
WaiveRyder <nzh.wolf@gmail.com>
2026-03-06 19:07:58 -0700

    Rewrote AuthDAO to be able to switch to map or sql database implementation based on constructor

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,26 +1,47 @@
 package dataaccess;
 
+import dataaccess.offline.AuthDAOMap;
 import model.AuthData;
 import model.UserData;
 
-public class AuthDAO {
+import java.util.Map;
 
-    public AuthDAO() {
+public class AuthDAO {
+    DatabaseManager database;
+    AuthDAOMap authDAOMap;
+    boolean useMap;
+
+    public AuthDAO(DatabaseManager database) {
+        this.database = database;
+        useMap = false;
+    }
+
+    public AuthDAO(Map<String, AuthData> authMap) {
+        authDAOMap = new AuthDAOMap(authMap);
+        useMap = true;
     }
 
     public AuthData createAuth(UserData user) {
-
+        if (useMap) {
+            return authDAOMap.createAuth(user);
+        }
     }
 
     public AuthData getAuthData(String token) throws DataAccessException {
-
+        if (useMap) {
+            return authDAOMap.getAuthData(token);
+        }
     }
 
     public void deleteAuthData(String token) throws DataAccessException {
-
+        if (useMap) {
+            authDAOMap.deleteAuthData(token);
+        }
     }
 
     public void clear() {
-
+        if (useMap) {
+            authDAOMap.clear();
+        }
     }
 }
