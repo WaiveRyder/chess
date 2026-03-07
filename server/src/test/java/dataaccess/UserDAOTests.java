f7b5e3dc95a48528326bc4c93be949e7cdc1a31c
WaiveRyder <nzh.wolf@gmail.com>
2026-03-07 14:51:39 -0700

    Implemented positive test case for get user

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,8 +14,6 @@ import java.sql.SQLException;
 public class UserDAOTests {
     UserDAO userDAO;
     AuthDAO authDAO;
-
-    AuthData mockUser;
 
     @BeforeEach
     public void setup() {
@@ -41,8 +40,8 @@ public class UserDAOTests {
             Assertions.assertNotNull(actual.password());
 
             var statement = "SELECT * FROM users WHERE username = ?";
-            try (var conn = DatabaseManager.getConnection();
-                 var pstmt = conn.prepareStatement(statement)) {
+            try (Connection conn = DatabaseManager.getConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(statement)) {
                 pstmt.setString(1, "John");
                 try (ResultSet rs = pstmt.executeQuery()) {
                     Assertions.assertTrue(rs.next());
@@ -72,12 +71,40 @@ public class UserDAOTests {
         }
 
         var statement = "SELECT * FROM users WHERE username = ?";
-        try (var conn = DatabaseManager.getConnection();
+        try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(statement)) {
             pstmt.setString(1, "John");
             try (ResultSet rs = pstmt.executeQuery()) {
                 Assertions.assertTrue(rs.next());
                 Assertions.assertFalse(rs.next());
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void getUserValidInfo() {
+        String expectedPassword = null;
+        try {
+            userDAO.createUser("John", "password", "email");
+            UserData actual = userDAO.getUser("John");
+            UserData expected = new UserData("John", actual.password(), "email");
+            expectedPassword = actual.password();
+            Assertions.assertEquals(expected, actual);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        var statement = "SELECT * FROM users WHERE username = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(statement)) {
+            pstmt.setString(1, "John");
+            try (ResultSet rs = pstmt.executeQuery()) {
+                Assertions.assertTrue(rs.next());
+                Assertions.assertEquals("John", rs.getString("username"));
+                Assertions.assertEquals(expectedPassword, rs.getString("password"));
+                Assertions.assertEquals("email", rs.getString("email"));
             }
         } catch (SQLException | DataAccessException e) {
             throw new RuntimeException(e);
