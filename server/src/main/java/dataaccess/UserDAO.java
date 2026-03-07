ac126c4048db4a8f24db8da375ea97a032f16af8
WaiveRyder <nzh.wolf@gmail.com>
2026-03-07 16:15:24 -0700

    Removed unnecessary brackets

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -28,31 +28,30 @@ public class UserDAO {
     public UserData createUser(String username, String password, String email) throws DataAccessException {
         if (useMap) {
             return userDAOMap.createUser(username, password, email);
-        } else {
-            try (Connection conn = DatabaseManager.getConnection()) {
-                var statement = "SELECT * FROM users WHERE username = ?";
+        }
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM users WHERE username = ?";
 
-                try (PreparedStatement check = conn.prepareStatement(statement)) {
-                    check.setString(1, username);
+            try (PreparedStatement check = conn.prepareStatement(statement)) {
+                check.setString(1, username);
 
-                    try (ResultSet rs = check.executeQuery()) {
-                        if (rs.next()) {
-                            throw new DataAccessException("Error: Database already contains username: " + username);
-                        }
-                        statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-                        try (PreparedStatement insert = conn.prepareStatement(statement) ) {
-                            String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-                            insert.setString(1, username);
-                            insert.setString(2, hashedPassword);
-                            insert.setString(3, email);
-                            insert.executeUpdate();
-                            return new UserData(username, hashedPassword, email);
-                        }
+                try (ResultSet rs = check.executeQuery()) {
+                    if (rs.next()) {
+                        throw new DataAccessException("Error: Database already contains username: " + username);
+                    }
+                    statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+                    try (PreparedStatement insert = conn.prepareStatement(statement) ) {
+                        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+                        insert.setString(1, username);
+                        insert.setString(2, hashedPassword);
+                        insert.setString(3, email);
+                        insert.executeUpdate();
+                        return new UserData(username, hashedPassword, email);
                     }
                 }
-            } catch (SQLException e) {
-                throw new DataAccessException("Error: could not connect to database");
             }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: could not connect to database");
         }
     }
 
