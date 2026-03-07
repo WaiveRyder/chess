195ab594f96f09235f44f91f62700302d1b8e535
WaiveRyder <nzh.wolf@gmail.com>
2026-03-07 13:53:05 -0700

    Implemented positive test for creating users

---
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -0,0 +1,72 @@
+package dataaccess;
+
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class AuthDAOTests {
+    AuthDAO authDAO;
+    UserDAO userDAO;
+    GameDAO gameDAO;
+
+    UserData mockUser;
+
+    @BeforeEach
+    public void setup() {
+        try {
+            DatabaseManager.createDatabase();
+            DatabaseManager.initTables();
+
+            authDAO = new AuthDAO();
+            userDAO = new UserDAO();
+            gameDAO = new GameDAO();
+
+            authDAO.clear();
+            userDAO.clear();
+            gameDAO.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            mockUser = userDAO.createUser("john", "password", "email");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void createAuthValidUserTest() {
+        try {
+            AuthData actual = authDAO.createAuth(mockUser);
+            AuthData expected = new AuthData(actual.authToken(), "john");
+
+            try (Connection conn = DatabaseManager.getConnection()) {
+                var statement = "SELECT * FROM auth WHERE token = ?";
+                try (PreparedStatement pstmt = conn.prepareStatement(statement)) {
+                    pstmt.setString(1, actual.authToken());
+                    try (ResultSet rs = pstmt.executeQuery()) {
+                        Assertions.assertTrue(rs.next());
+                        Assertions.assertEquals("john", rs.getString("username"));
+                        Assertions.assertEquals(actual.authToken(), rs.getString("token"));
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            Assertions.assertEquals(expected, actual);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
