43df7eb4af87d1f02b26cb5f1361b7ec96934bc9
WaiveRyder <nzh.wolf@gmail.com>
2026-03-22 11:04:40 -0600

    Added http client to serverfacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,9 +1,13 @@
 package client;
 
+import java.net.http.HttpClient;
+
 public class ServerFacade {
     int port;
+    HttpClient client;
 
     public ServerFacade(int port) {
         this.port = port;
+        client = HttpClient.newHttpClient();
     }
 }
