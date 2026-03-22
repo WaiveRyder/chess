63981d57e586c4fa7423b1d31d715f3f1c259ac7
WaiveRyder <nzh.wolf@gmail.com>
2026-03-22 11:32:27 -0600

    Implemented Help command on pre-login

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -8,19 +8,24 @@ public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("Welcome to Chess! Type 'help' for a list of commands.");
 
         Scanner scanner = new Scanner(System.in);
+
         ServerFacade serverFacade = new ServerFacade(8080);
+        ClientDraw clientDraw = new ClientDraw();
+        State state = State.PRE_LOGIN;
 
         while (true) {
-            System.out.println("Running REPL. Type 'exit' to quit.");
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("exit")) {
-                System.out.println("Exiting...");
+            if (input.equalsIgnoreCase("help")) {
+                clientDraw.draw(input, state);
+            } else if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Thanks for playing! Catch you next time.");
                 break;
             } else {
-                System.out.println("I heard: " + input);
+                System.out.println("I didn't quite catch that. Type 'help' for a list of commands.");
             }
         }
 
