cf6665e447284305251b87904dd7076bf2595582
WaiveRyder <nzh.wolf@gmail.com>
2026-03-22 10:58:55 -0600

    Implemented elementary repl

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,25 @@ package client;
 
 import chess.*;
 
+import java.util.Scanner;
+
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Running REPL. Type 'exit' to quit.");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Exiting...");
+                break;
+            } else {
+                System.out.println("I heard: " + input);
+            }
+        }
     }
 }
