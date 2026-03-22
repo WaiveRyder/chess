f0a8ee839931bd3d7d131a049cfdf797b3653c3e
WaiveRyder <nzh.wolf@gmail.com>
2026-03-22 12:02:11 -0600

    Added all blank functions to handle http requests

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -14,7 +14,29 @@ public class ServerFacade {
     public void request(String... args) {
         if (args.length == 0) {
             ClientDraw.printError("No command provided");
+        } else {
+            switch (args[0].toLowerCase()) {
+                case "login" -> loginHandler();
+                case "register" -> registerHandler();
+                case "list" -> listHandler();
+                case "join" -> joinHandler();
+                case "create" -> createHandler();
+                case "observe" -> observeHandler();
+                case "logout" -> logoutHandler();
+                case "leave" -> leaveHandler();
+                default -> ClientDraw.printError("Unknown command: " + args[0]);
+            }
         }
     }
+
+    private void loginHandler() {}
+    private void registerHandler() {}
+    private void listHandler() {}
+    private void joinHandler() {}
+    private void createHandler() {}
+    private void observeHandler() {}
+    private void logoutHandler() {}
+    private void leaveHandler() {}
+
 
 }
