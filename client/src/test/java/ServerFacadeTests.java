7606861abf28d85f6411d2c7b3233f5213742774
WaiveRyder <nzh.wolf@gmail.com>
2026-03-23 20:07:36 -0600

    Test help pos

--- client/src/test/java/ServerFacadeTests.java
+++ client/src/test/java/ServerFacadeTests.java
@@ -1,10 +1,20 @@
+import client.ServerFacade;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+
+    ServerFacade serverFacade;
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeAll
     public static void init() {
@@ -13,15 +23,38 @@ public class ServerFacadeTests {
         System.out.println("Started test HTTP server on " + port);
     }
 
+    @BeforeEach
+    public void setup() {
+        serverFacade = new ServerFacade(0);
+
+        System.setOut(new PrintStream(outputStreamCaptor, true, StandardCharsets.UTF_8));
+    }
+
     @AfterAll
     static void stopServer() {
         server.stop();
     }
 
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void testHelp() {
+        String exptected = """
+                Commands:
+                - login <username> <password>: Login with username and password
+                - register <username> <password> <email>: Register with username, password, and email
+                - quit: Exit the application
+                - help: Lists all commands
+                """.stripIndent();
+        serverFacade.request("help");
+
+        Assertions.assertEquals(exptected, outputStreamCaptor.toString().strip());
     }
+
+
 
 }
