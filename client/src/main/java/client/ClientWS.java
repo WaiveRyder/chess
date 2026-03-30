89e485826e3f05c1ad8d8ea2820783a488958398
WaiveRyder <nzh.wolf@gmail.com>
2026-03-29 18:57:05 -0600

    Implemented sending messages through the clientWS

--- client/src/main/java/client/ClientWS.java
+++ client/src/main/java/client/ClientWS.java
@@ -1,17 +1,22 @@
 package client;
+import com.google.gson.Gson;
 import jakarta.websocket.*;
+import websocket.commands.UserGameCommand;
 
+import java.net.ConnectException;
 import java.net.URI;
 
 @ClientEndpoint
 public class ClientWS {
     private Session session;
+    Gson gson;
 
     public ClientWS(int port) {
         try {
             URI uri = new URI("ws://localhost:" + port + "/ws");
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             session = container.connectToServer(this, uri);
+            gson = new Gson();
         } catch (Exception e) {
             System.out.println("Error creating URI: " + e.getMessage());
         }
@@ -29,12 +34,33 @@ public class ClientWS {
         System.out.println("Received message: " + message);
     }
 
-    public void sendMessage(String message) {
+    private void sendMessage(
+            UserGameCommand.CommandType type,
+            String authToken,
+            Integer gameID
+    ) throws ConnectException {
         if (session != null && session.isOpen()) {
-            session.getAsyncRemote().sendText(message);
+            UserGameCommand command= new UserGameCommand(type, authToken, gameID);
+            session.getAsyncRemote().sendText(gson.toJson(command));
         } else {
-            System.out.println("Error: Websocket is not open. Cannot send message.");
+            throw new ConnectException("Error: Websocket session is not open.");
         }
+    }
+
+    public void connect(String authToken, Integer gameID) throws ConnectException {
+        sendMessage(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+    }
+
+    public void makeMove(String authToken, Integer gameID) throws ConnectException {
+        sendMessage(UserGameCommand.CommandType.MAKE_MOVE, authToken, gameID);
+    }
+
+    public void leave(String authToken, Integer gameID) throws ConnectException {
+        sendMessage(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+    }
+
+    public void resign(String authToken, Integer gameID) throws ConnectException {
+        sendMessage(UserGameCommand.CommandType.RESIGN, authToken, gameID);
     }
 
     public void close() {
