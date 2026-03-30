97e376936782423266c1b68f701ffcd0318d9fbe
WaiveRyder <nzh.wolf@gmail.com>
2026-03-29 19:25:36 -0600

    Added message to user game command

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -16,10 +16,19 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
+    private String message;
+
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+    }
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, String message) {
+        this.commandType = commandType;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.message = message;
     }
 
     public enum CommandType {
@@ -39,6 +48,10 @@ public class UserGameCommand {
 
     public Integer getGameID() {
         return gameID;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     @Override
